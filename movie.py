f588cf40c9ae07abbfea445e491f9fce7a490bd8
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-05-01 15:08:11 -0500

    Lab9 2

--- movie.py
+++ movie.py
@@ -5,18 +5,14 @@ class Movie:
         self.title = title
         self.director = director
         self.year = year
-# FREEZE CODE END
-    # TODO: Define the __str__ method!
-
-
-# FREEZE CODE BEGIN
+    def __str__(self):
+        return f"Movie: {self.tittle} (Directed by {self.director}, {self.year})"
+    
 if __name__ == "__main__":
     # --- Main Program ---
     title = input("Enter the movie title: ")
     director = input("Enter the director's name: ")
     year = input("Enter the release year: ")
-# FREEZE CODE END
-    
-    
-    # TODO: Construct a Movie object!
-    # TODO: Print the object!
+
+movie = Movie(title, director, year)
+print(movie)
